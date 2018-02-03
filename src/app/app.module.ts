24addc81c8433ccbf98f4e75c55f8583d2a36398
haleem737 <gmmkk2009@gmail.com>
2018-02-03 07:46:16 +0300

    try this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { Home2Component } from './components/home2/home2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutUsComponent,
+    Home2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '' , component: HomeComponent },
+      { path: 'about-us' , component: AboutUsComponent },
+      { path: 'home2' , component: Home2Component },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/about-us/about-us.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
+})
+export class AboutUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    $('.test').css('background-color', '#661266');
+  }
+
+}
